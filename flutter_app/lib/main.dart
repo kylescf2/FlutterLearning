dab6c4fae22d2d3dcc47add3124939ba98521f46
kyle <kaikaiscf2@126.com>
2019-06-28 23:03:49 +0800

    MOD: Container 使用

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'shopping_list.dart';
 
-//void main() => runApp(new MyApp());
+void main() => runApp(new FooApp());
 
 // TODO: test
 //void main() {
@@ -18,180 +18,19 @@ import 'shopping_list.dart';
 //  ));
 //}
 
-void main() {
-  runApp(CupertinoApp(
-    home: new Container(
-        decoration: new BoxDecoration(
-            border: new Border.all(width: 2.0, color: Colors.red),
-            borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
-            color: Colors.grey
-        ),
-        child: buildRect()
-    )
-  ));
+
+class FooApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+        title: 'Flutter layout demo',
+        home: new Container(
+            decoration: new BoxDecoration(color: Colors.white),
+            child: buildContainer(context)
+        )
+    );
+  }
 }
-
-///
-
-//class MyApp extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'Flutter layout demo',
-//      home: buildHomePage('Strawberry Pavlova Recipe'),
-//    );
-//  }
-//
-//  Widget buildHomePage(String title) {
-//    final titleText = Container(
-//      padding: EdgeInsets.all(20),
-//      child: Text(
-//        'Strawberry Pavlova',
-//        style: TextStyle(
-//          fontWeight: FontWeight.w800,
-//          letterSpacing: 0.5,
-//          fontSize: 30,
-//        ),
-//      ),
-//    );
-//
-//    final subTitle = Text(
-//      'Pavlova is a meringue-based dessert named after the Russian ballerina '
-//          'Anna Pavlova. Pavlova features a crisp crust and soft, light inside, '
-//          'topped with fruit and whipped cream.',
-//      textAlign: TextAlign.center,
-//      style: TextStyle(
-//        fontFamily: 'Georgia',
-//        fontSize: 25,
-//      ),
-//    );
-//
-//    // #docregion ratings, stars
-//    var stars = Row(
-//      mainAxisSize: MainAxisSize.min,
-//      children: [
-//        Icon(Icons.star, color: Colors.green[500]),
-//        Icon(Icons.star, color: Colors.green[500]),
-//        Icon(Icons.star, color: Colors.green[500]),
-//        Icon(Icons.star, color: Colors.black),
-//        Icon(Icons.star, color: Colors.black),
-//      ],
-//    );
-//    // #enddocregion stars
-//
-//    final ratings = Container(
-//      padding: EdgeInsets.all(20),
-//      child: Row(
-//        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//        children: [
-//          stars,
-//          Text(
-//            '170 Reviews',
-//            style: TextStyle(
-//              color: Colors.black,
-//              fontWeight: FontWeight.w800,
-//              fontFamily: 'Roboto',
-//              letterSpacing: 0.5,
-//              fontSize: 20,
-//            ),
-//          ),
-//        ],
-//      ),
-//    );
-//    // #enddocregion ratings
-//
-//    // #docregion iconList
-//    final descTextStyle = TextStyle(
-//      color: Colors.black,
-//      fontWeight: FontWeight.w800,
-//      fontFamily: 'Roboto',
-//      letterSpacing: 0.5,
-//      fontSize: 18,
-//      height: 2,
-//    );
-//
-//    // DefaultTextStyle.merge() allows you to create a default text
-//    // style that is inherited by its child and all subsequent children.
-//    final iconList = DefaultTextStyle.merge(
-//      style: descTextStyle,
-//      child: Container(
-//        padding: EdgeInsets.all(20),
-//        child: Row(
-//          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//          children: [
-//            Column(
-//              children: [
-//                Icon(Icons.kitchen, color: Colors.green[500]),
-//                Text('PREP:'),
-//                Text('25 min'),
-//              ],
-//            ),
-//            Column(
-//              children: [
-//                Icon(Icons.timer, color: Colors.green[500]),
-//                Text('COOK:'),
-//                Text('1 hr'),
-//              ],
-//            ),
-//            Column(
-//              children: [
-//                Icon(Icons.restaurant, color: Colors.green[500]),
-//                Text('FEEDS:'),
-//                Text('4-6'),
-//              ],
-//            ),
-//          ],
-//        ),
-//      ),
-//    );
-//    // #enddocregion iconList
-//
-//    // #docregion leftColumn
-//    final leftColumn = Container(
-//      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
-//      child: Column(
-//        children: [
-//          titleText,
-//          subTitle,
-//          ratings,
-//          iconList,
-//        ],
-//      ),
-//    );
-//    // #enddocregion leftColumn
-//
-//    final mainImage = Image.asset(
-//      'images/pavlova.jpg',
-//      fit: BoxFit.cover,
-//    );
-//
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text(title),
-//      ),
-//      // #docregion body
-//      body: Center(
-//        child: Container(
-//          margin: EdgeInsets.fromLTRB(0, 250, 0, 30),
-//          height: 600,
-//          child: Card(
-//            child: Row(
-//              crossAxisAlignment: CrossAxisAlignment.start,
-//              children: [
-//                Container(
-//                  width: 440,
-//                  child: leftColumn,
-//                ),
-//                mainImage,
-//              ],
-//            ),
-//          ),
-//        ),
-//      ),
-//      // #enddocregion body
-//    );
-//  }
-//}
 
 class Counter extends StatefulWidget {
   @override
@@ -221,13 +60,13 @@ class _CounterState extends State<Counter> {
 
 Widget buildRow() {
   return Row(
+//    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     mainAxisSize: MainAxisSize.min,
+    crossAxisAlignment: CrossAxisAlignment.center,
     children: [
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.green[500]),
+      Image.asset('images/pic1.jpg'),
+      Image.asset('images/pic2.jpg'),
+      Image.asset('images/pic3.jpg'),
     ],
   );
 }
@@ -243,22 +82,25 @@ Widget buildColumn() {
   );
 }
 
-Widget buildRect() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.end,
-    crossAxisAlignment: CrossAxisAlignment.end,
-    children: <Widget>[
-      const SizedBox(width: 10.0),
-      Text("Hello World!"),
-      const SizedBox(width: 20.0),
-      SizedBox(
-        width: 150,
-        height: 150,
-        child: Container(
-          color: Colors.blue,
-          child: Image.asset("images/pic1.jpg"),
-        )
-      ),
-    ],
+Widget buildContainer(BuildContext context) {
+  var container_test = Container(
+    constraints: new BoxConstraints.expand(
+      height:Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
+    ),
+    decoration: new BoxDecoration(
+      border: new Border.all(width: 2.0, color: Colors.red),
+      color: Colors.grey,
+      borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
+//      image: new DecorationImage(
+//        image: new NetworkImage('http://h.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=0d023672312ac65c67506e77cec29e27/9f2f070828381f30dea167bbad014c086e06f06c.jpg'),
+//        centerSlice: new Rect.fromLTRB(270.0, 180.0, 1360.0, 730.0),
+//      ),
+    ),
+    padding: const EdgeInsets.all(8.0),
+    alignment: Alignment.center,
+    child: new Text('Hello World',
+        style: Theme.of(context).textTheme.display1.copyWith(color: Colors.black)),
   );
+
+  return container_test;
 }
