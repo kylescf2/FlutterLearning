a2874e056924277334ef915335e663e6c76bf4c2
kyle <kaikaiscf2@126.com>
2019-06-29 14:32:31 +0800

    ADD: flutter_gallery demo

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -24,10 +24,26 @@ class FooApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoApp(
         title: 'Flutter layout demo',
-        home: new Container(
-            decoration: new BoxDecoration(color: Colors.white),
-            child: buildContainer(context)
+        home: new Stack(
+          children: <Widget>[Positioned(
+              left: 40,
+              bottom: 10,
+//            decoration: new BoxDecoration(color: Colors.white),
+              child: buildContainer(context)
+          ),
+
+          Positioned(
+              left: 40,
+              bottom: 100,
+//            decoration: new BoxDecoration(color: Colors.white),
+              child: buildContainer(context)
+          )
+
+          ],
         )
+
+
+
     );
   }
 }
@@ -84,22 +100,9 @@ Widget buildColumn() {
 
 Widget buildContainer(BuildContext context) {
   var container_test = Container(
-    constraints: new BoxConstraints.expand(
-      height:Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
-    ),
-    decoration: new BoxDecoration(
-      border: new Border.all(width: 2.0, color: Colors.red),
-      color: Colors.grey,
-      borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
-//      image: new DecorationImage(
-//        image: new NetworkImage('http://h.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=0d023672312ac65c67506e77cec29e27/9f2f070828381f30dea167bbad014c086e06f06c.jpg'),
-//        centerSlice: new Rect.fromLTRB(270.0, 180.0, 1360.0, 730.0),
-//      ),
-    ),
-    padding: const EdgeInsets.all(8.0),
-    alignment: Alignment.center,
-    child: new Text('Hello World',
-        style: Theme.of(context).textTheme.display1.copyWith(color: Colors.black)),
+    width: 200,
+    height: 10.0,
+    decoration: BoxDecoration(color: Colors.red),
   );
 
   return container_test;
